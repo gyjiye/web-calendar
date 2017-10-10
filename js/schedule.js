4d5961ef4345135dc1d8aea6db43813427d8059a
humoufeng <humoufeng@gmail.com>
2017-10-10 21:47:02 +0800

    finish schedule page stylesheet
    finish schedule page stylesheet and start working on schedule.js
    fix bootstrap-switch plugin

---
+++ js/schedule.js
@@ -0,0 +1,10 @@
+// plugin init
+$(function() {
+    //    init the scroller
+    $(".nano").nanoScroller();
+    //    init the textarea for flexText
+    $('textarea').flexText();
+    //    init the bootstrapSwitch checkbox;
+    $('input[name="my-checkbox"]').bootstrapSwitch({});
+});
+
