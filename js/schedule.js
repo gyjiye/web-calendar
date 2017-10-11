93253bfc2464e3d9117e8f474bcf924cb400343f
humoufeng <humoufeng@gmail.com>
2017-10-12 00:46:40 +0800

    finish the behaviour between description and thing-list
    make it to travel between description and things list, make sure to init the page structure when js media query

--- js/schedule.js
+++ js/schedule.js
@@ -1,5 +1,9 @@
 // ===============global variables=============================
-var size = 1; // window size with 3 for large, 2 for middle, 1 for small
+var size = 1,// window size with 3 for large, 2 for middle, 1 for small
+    things_list = $('.things-list'), // things list
+    description = $('.description'), // description
+    part_share = $('.part-share'), //description part-share
+    des_close = $('.part-des .close'); // description des-part close
 
 
 // =============== END global variables=============================
@@ -18,13 +22,27 @@ $(function() {
 // =============== END plugin init=============================
 
 // =============== media query =============================
+// page init with different screen size
+function page_init_large() {
+    description.show();
+    description.css('width', '40%');
+    des_close.hide();
+}
+
+function page_init_middle() {
+    description.css('width', '65%');
+    des_close.show();
+}
+
 $(window).resize(function () {
     var windowWidth = $(window).width();
     console.log(windowWidth);
     if(windowWidth >= 1200) {
         size = 3;
+        page_init_large();
     }else if (windowWidth >= 650) {
         size =  2;
+        page_init_middle();
     } else {
         size = 1;
     }
@@ -38,19 +56,15 @@ $(window).resize(function () {
 (function () {
    var share = $('#share-button');
     function move_to_sharepage() {
-        var part_share = $('.part-share');
         if(!part_share.hasClass('part-share-show')){
             part_share.addClass('part-share-show');
         }
-        else {
-            console.log('part-share already have class part-share-show');
-        }
+        else { console.log('part-share already have class part-share-show') }
     }
    share.on('click', move_to_sharepage);
 })();
 
 function move_away_sharepage() {
-    var part_share = $('.part-share');
     if(part_share.hasClass('part-share-show')){
         part_share.removeClass('part-share-show');
     }
@@ -65,19 +79,27 @@ function move_away_sharepage() {
     share_close.on('click', move_away_sharepage);
 })();
 
+function move_away_despage() {
+    description.hide();
+    things_list.show();
+}
+
+// des close
+(function () { des_close.on('click', move_away_despage); })();
+
 // =========================================
 // on middle size windows
 
 // click task-item to call description
 (function () {
     var task_item_middle = $('.task-item');
-    var description = $('.description');
     function move_to_despage() {
-        // console.log("click");
-        var things_list = $('.things-list');
-        things_list.hide();
-        description.show();
-        description.css('width', '65%');
+        if(size < 3) {
+            // only works on middle and small screen
+            // console.log("click");
+            things_list.hide();
+            description.show();
+        }
     };
 
     task_item_middle.on('click', move_to_despage);
